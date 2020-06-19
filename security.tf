141bd1f283c27fb0855fb959ecee29be8f5a9dad
Kevin Corbin <kcorbin@hashicorp.com>
2020-06-19 15:44:03 -0500

    update security groups

--- security.tf
+++ security.tf
@@ -24,6 +24,34 @@ resource "aws_security_group" "bastionsg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8301
+    to_port     = 8301
+    protocol    = "tcp"
+    cidr_blocks = ["172.25.16.0/20"]
+  }
+
+  ingress {
+    from_port   = 8301
+    to_port     = 8301
+    protocol    = "udp"
+    cidr_blocks = ["172.25.16.0/20"]
+  }
+
+  ingress {
+    from_port   = 8301
+    to_port     = 8301
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    from_port   = 8301
+    to_port     = 8301
+    protocol    = "udp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
   egress {
     from_port       = 0
     to_port         = 0
